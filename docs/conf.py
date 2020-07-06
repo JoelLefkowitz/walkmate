38229ed011fb99ac4d9076e21db476dc3d2415d6
Joel Lefkowitz <joellefkowitz@hotmail.com>
2020-07-06 21:55:44 +0100

    Bump version: 0.1.0 → 1.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@ path_guard("../..")
 
 
 project = "Walker"
-version = "0.1.0"
+version = "1.0.0"
 master_doc = "index"
 author = "Joel Lefkowitz"
 copyright = f"{datetime.datetime.now().year}, {author}"
