108ccb05d9cb40c74ec2861121fce0e4b53c24ee
Joel Lefkowitz <joellefkowitz@hotmail.com>
2020-07-30 17:43:33 +0100

    Bump version: 1.2.0 → 1.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -10,7 +10,7 @@ path_guard("../..")
 
 
 project = "walkmate"
-version = "1.2.0"
+version = "1.2.1"
 master_doc = "index"
 author = "Joel Lefkowitz"
 copyright = f"{datetime.datetime.now().year}, {author}"
